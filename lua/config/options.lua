33535885649479d765fdcd97d91b3b2f801ee69d
theRunCom <1772414249@qq.com>
2024-03-28 09:12:20 +0800

    Update neovide config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,11 @@
 if vim.g.neovide then
   vim.o.guifont = "JetBrains Mono,LiSu:h20"
   vim.g.neovide_cursor_vfx_mode = "ripple"
-  vim.g.neovide_transparency = 0.9
+  -- vim.g.neovide_transparency = 0.9
   vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
 local opt = vim.opt
